4d1494230cb959f4b3646460866b255dc257ee5b
lilingj <1020305202@qq.com>
2021-03-16 10:53:12 +0800

    Start or close mysql and tools in init

--- src/init/init.go
+++ src/init/init.go
@@ -3,16 +3,26 @@ package init
 import (
 	"mictract/global"
 	"mictract/model"
+	"mictract/model/kubernetes"
+	"time"
 )
 
 func init() {
 	// initialization code goes here.
+	(&kubernetes.Tools{}).Create()
+	(&kubernetes.Mysql{}).Create()
+	// TODO: sync
+	time.Sleep(5 * time.Second)
+
 	createTables()
 	initNetsAndSDKs()
 }
 
 func Close() {
 	model.UpsertAllNets()
+
+	(&kubernetes.Tools{}).Delete()
+	(&kubernetes.Mysql{}).Delete()
 	//for _, net := range global.Nets {
 	//	n := net.(model.Network)
 	//	if err := n.Insert(); err != nil {
