fd3de9d701e2183752c7324dd7962067475e95b7
lilingj <1020305202@qq.com>
2021-04-15 14:59:06 +0800

    fix: add src/dao

---
+++ src/dao/channel.go
@@ -0,0 +1,72 @@
+package dao
+
+import (
+	"github.com/pkg/errors"
+	"mictract/global"
+	"mictract/model"
+)
+
+func InsertChannel(ch *model.Channel) error {
+	return global.DB.Create(ch).Error
+}
+
+func FindChannelByID(chID int) (*model.Channel, error) {
+	var chs []model.Channel
+	if err := global.DB.Where("id = ?", chID).Find(&chs).Error; err != nil {
+		return &model.Channel{}, err
+	}
+	if len(chs) == 0 {
+		return &model.Channel{}, errors.New("channel not found")
+	}
+	return &chs[0], nil
+}
+
+func UpdateOrgIDs(chID, orgID int) error {
+	// 加个互斥锁
+	global.ChannelLock.Lock()
+	defer global.ChannelLock.Unlock()
+
+	ch, err := FindChannelByID(chID)
+	if err != nil {
+		return err
+	}
+
+	ch.OrganizationIDs = append(ch.OrganizationIDs, orgID)
+
+	return global.DB.Model(ch).Update("organization_ids", ch.OrganizationIDs).Error
+}
+
+func UpdateChannelStatusByID(chID int, status string) error {
+	return global.DB.Model(&model.Channel{}).Where("id = ?", chID).Update("status", status).Error
+}
+
+func FindAllOrganizationsInChannel(c *model.Channel) ([]model.Organization, error) {
+	if len(c.OrganizationIDs) <= 0 {
+		return []model.Organization{}, errors.New("no organization in channel")
+	}
+	orgs := []model.Organization{}
+	for _, orgID := range c.OrganizationIDs {
+		org, err := FindOrganizationByID(orgID)
+		if err != nil {
+			return []model.Organization{}, err
+		}
+		orgs = append(orgs, *org)
+	}
+	return orgs, nil
+}
+
+func FindAllPeersInChannel(c *model.Channel) ([]model.CaUser, error) {
+	peers := []model.CaUser{}
+	for _, orgID := range c.OrganizationIDs {
+		_peers, err := FindAllPeersInOrganization(orgID)
+		if err != nil {
+			return []model.CaUser{}, err
+		}
+		peers = append(peers, _peers...)
+	}
+	return peers, nil
+}
+
+func DeleteAllChannelsInNetwork(netID int) error {
+	return global.DB.Where("network_id = ?", netID).Delete(&model.Channel{}).Error
+}
---
+++ src/dao/network.go
@@ -0,0 +1,93 @@
+package dao
+
+import (
+	"github.com/pkg/errors"
+	"mictract/global"
+	"mictract/model"
+)
+
+func InsertNetwork(net *model.Network) error {
+	return global.DB.Create(net).Error
+}
+
+func FindAllNetworks() ([]model.Network, error){
+	nets := []model.Network{}
+	if err := global.DB.Find(&nets).Error; err != nil {
+		return nil, errors.WithMessage(err, "Fail to query all nets")
+	}
+	return nets, nil
+}
+
+func FindNetworkByID(id int) (*model.Network, error) {
+	var nets []model.Network
+	if err := global.DB.Where("id = ?", id).Find(&nets).Error; err != nil {
+		global.Logger.Error(err.Error())
+		return &model.Network{}, err
+	}
+	if len(nets) == 0 {
+		return &model.Network{}, errors.New("no such network")
+	}
+	return &nets[0], nil
+}
+
+func FindAllPeersInNetwork(netID int) ([]model.CaUser, error)  {
+	cus := []model.CaUser{}
+	if err := global.DB.Where("network_id = ? and type = ?", netID, "peer").Find(&cus).Error; err != nil {
+		return []model.CaUser{}, err
+	}
+	if len(cus) == 0 {
+		return []model.CaUser{}, errors.New("no peer in network")
+	}
+	return cus, nil
+}
+
+func FindAllOrderersInNetwork(netID int) ([]model.CaUser, error)  {
+	cus := []model.CaUser{}
+	if err := global.DB.Where("network_id = ? and type = ?", netID, "orderer").Find(&cus).Error; err != nil {
+		return []model.CaUser{}, err
+	}
+	if len(cus) == 0 {
+		return []model.CaUser{}, errors.New("no orderer in network")
+	}
+	return cus, nil
+}
+
+func FindAllOrganizationsInNetwork(netID int) ([]model.Organization, error) {
+	orgs := []model.Organization{}
+	if err := global.DB.Where("network_id = ?", netID).Find(&orgs).Error; err != nil {
+		return []model.Organization{}, err
+	}
+	return orgs, nil
+}
+
+func FindOrdererOrganizationInNetwork(netID int) (*model.Organization, error) {
+	orgs := []model.Organization{}
+	if err := global.DB.Where("network_id = ? and is_orderer_org = ?", netID, true).Find(&orgs).Error; err != nil {
+		return &model.Organization{}, err
+	}
+	return &orgs[0], nil
+}
+
+func FindAllChannelsInNetwork(netID int) ([]model.Channel, error) {
+	chs := []model.Channel{}
+	if err := global.DB.Where("network_id = ?", netID).Find(&chs).Error; err != nil {
+		return []model.Channel{}, err
+	}
+	return chs, nil
+}
+
+func FindAllChaincodesInNetwork(netID int) ([]model.Chaincode, error) {
+	ccs := []model.Chaincode{}
+	if err := global.DB.Where("network_id = ?", netID).Find(&ccs).Error; err != nil {
+		return []model.Chaincode{}, err
+	}
+	return ccs, nil
+}
+
+func UpdateNetworkStatusByID(id int, status string) error {
+	return global.DB.Model(&model.Network{}).Where("id = ?", id).Update("status", status).Error
+}
+
+func DeleteNetworkByID(netID int) error {
+	return global.DB.Where("id = ?", netID).Delete(&model.Network{}).Error
+}
